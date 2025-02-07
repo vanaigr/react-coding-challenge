bac83413cd694f03140d6aaa071f9918fcaa6929
vanaigr <vanaigranov@gmail.com>
2025-02-06 22:24:40 -0600

    feat: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    { path: '/', element: <div>dashboard</div> },
+    { path: '/equipment', element: <div>equipment table</div> },
+    { path: '/equipment/:id', element: <div>equipment record</div> },
+    { path: '/maintenance', element: <div>maintenance table</div> },
+    { path: '/maintenance/:id', element: <div>maintenance record</div> },
+    { path: '*', element: <div>404</div> },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <StrictMode><div>Hello, internet!</div></StrictMode>
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>
 )
