2478d4ceeb5dbdcfa46ab792ffe862b31d3a4b01
vanaigr <vanaigranov@gmail.com>
2025-02-06 22:15:18 -0600

    feat: project setup

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <StrictMode><div>Hello, internet!</div></StrictMode>
+)
