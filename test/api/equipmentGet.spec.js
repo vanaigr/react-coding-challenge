84ae3711c61a9caa46c49b80690b5019a6b37bf0
vanaigr <vanaigranov@gmail.com>
2025-02-18 00:03:52 -0600

    test(api): equipment get by id

--- test/api/equipmentGet.spec.js
+++ test/api/equipmentGet.spec.js
@@ -7,12 +7,13 @@ import {
     expectJson,
     expectApiArr,
     resetDB,
+    equipmentValidation,
     equipmentValidationWithoutId,
 } from './common.js'
 
-describe('Equipment list', () => {
+describe('Getting equipment', () => {
     beforeEach(resetDB)
-    it('Can get all the equipment', async() => {
+    it('All', async() => {
         const res = await expectJson(api('equipment', 'GET'), 200)
         expectApiArr(res)
         const d = res.data
@@ -22,5 +23,28 @@ describe('Equipment list', () => {
             const r = equipmentValidationWithoutId.safeParse(d[i])
             expect(r.error).withContext('i=' + i).toBeFalsy()
         }
+    })
+
+    it('By id', async() => {
+        const id = 'f5c5285c-0af9-4502-8a4c-9e6682a04695'
+
+        const res = await expectJson(api(
+            'equipment/' + encodeURIComponent(id),
+            'GET'
+        ), 200)
+        expect(res.ok).withContext(JSON.stringify(res)).toBeTrue()
+        const valid = equipmentValidation.safeParse(res.data)
+        expect(valid.error).toBeUndefined()
+        expect(valid.data?.id).toEqual(id)
+    })
+
+    it('Rejects if does not exist', async() => {
+        const id = 'does-not-exist'
+
+        const res = await expectJson(api(
+            'equipment/' + encodeURIComponent(id),
+            'GET'
+        ), 200)
+        expect(res.ok).withContext(JSON.stringify(res)).not.toBeTrue()
     })
 })
